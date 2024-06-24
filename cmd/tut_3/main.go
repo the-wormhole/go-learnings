a4bdcf7017acd0c93cffd07e10a62eff801b0d86
the-wormhole <nayan.agg13@gmail.com>
2024-06-24 21:37:12 +0530

    Control Statements and Functions

--- cmd/tut_3/main.go
+++ cmd/tut_3/main.go
@@ -1,10 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	var strPrint string = "You printed me!!"
 	printString(strPrint)
+
+	var temp1 int = 8
+	var temp2 int = 6
+	var resultDivision int = intDiv(temp1, temp2)
+
+	fmt.Println(resultDivision)
+	var resultRem int
+	resultDivision, resultRem = retDivAndRem(temp1, temp2)
+
+	fmt.Println(resultDivision, resultRem)
+
+	fmt.Printf("Division result %v and remander is %v", resultDivision, resultRem)
+
+	div, rem, err := retDivAndRemAndError(temp1, temp2) //Directly declared and assigend
+	// if err != nil {
+	// 	fmt.Printf(err.Error())
+	// } else if rem == 0 {
+	// 	fmt.Printf("\nDivision result %v", div)
+	// } else {
+	// 	fmt.Printf("\nDivision result %v and remander is %v", div, rem)
+	// }
+
+	switch { // Normal Switch statements in go
+	case err != nil:
+		fmt.Printf(err.Error())
+	case rem == 0:
+		fmt.Printf("\nDivision result %v", div)
+	default:
+		fmt.Printf("\nDivision result %v and remander is %v", div, rem)
+	}
+	switch rem { // Conditional Switch statements in go, like in other languages
+	case 0:
+		fmt.Printf("\nAccurate Division")
+	case 1, 2:
+		fmt.Printf("\nDivision was close")
+	default:
+		fmt.Printf("\nDivision was not close")
+	}
 }
 
 func printString(str string) {
@@ -16,4 +57,18 @@ func intDiv(num int, den int) int {
 	fmt.Println(res)
 
 	return res
+}
+
+func retDivAndRem(num int, den int) (int, int) { // In go we can return multiple values at the same time
+	return num / den, num % den
+}
+
+func retDivAndRemAndError(num int, den int) (int, int, error) {
+	var err error //Default value is 'nil'
+	if den == 0 {
+		err = errors.New("Cannot Divide by Zero")
+
+		return 0, 0, err
+	}
+	return num / den, num % den, err
 }
