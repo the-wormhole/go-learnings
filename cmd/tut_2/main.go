12cac71bbc14f54969451eae3e47f0ed45cee470
the-wormhole <nayan.agg13@gmail.com>
2024-06-24 19:59:26 +0530

    Learning Go initiated

---
+++ cmd/tut_2/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// temp := 37
+
+	// var temp float32 = 3276758.6
+	var temp1 int = 255
+	//int32
+	//int64
+	//uint16
+	//uint32
+	//uint64
+	// fmt.Println(temp1)
+	var temp2 float32 = 140
+
+	//fmt.Println(temp1 + temp2) // Not possible due to different types.
+}
